ef4303cd99f031b6f9239c90d819d878edf69409
张立涛 <zhanglt@qiezzi.com>
2018-05-13 19:27:16 +0800

    实现控件的拖拽--scrollTo或者ScrollBy是对内容的拖动，setX或者setY是对控件位置的重置

--- app/src/main/java/test/com/selfdefineview/constraint/ConstraintActivity.java
+++ app/src/main/java/test/com/selfdefineview/constraint/ConstraintActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import test.com.selfdefineview.R;
@@ -29,7 +31,48 @@ public class ConstraintActivity extends AppCompatActivity {
         activity_main = findViewById(R.id.activity_main);
     }
 
-    public void floatBtton(View view){
+    public void floatBtton(final View view){
         view.setBackgroundColor(getResources().getColor(R.color.red_oa));
+        view.setOnTouchListener(new View.OnTouchListener() {
+            float dx;
+            float dy;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        dx = event.getRawX();
+                        dy = event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        /*view.setX(event.getRawX() - dx);
+                        view.setY(event.getRawY()- dy - getStatusBarHeight() - getTitleBarHeight());*/
+                        view.setX(event.getRawX() - dx);
+                        view.setY(event.getRawY()- dy);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+    /**
+     * 获取状态栏的高度
+     * @return
+     */
+    public int getStatusBarHeight(){
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight;
+    }
+    /**
+     * 获取布局标题头部的高度
+     * @return
+     */
+    public int getTitleBarHeight(){
+        int contentViewTop = getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        int titleBarHeight = contentViewTop - getStatusBarHeight();
+        return titleBarHeight;
     }
 }
