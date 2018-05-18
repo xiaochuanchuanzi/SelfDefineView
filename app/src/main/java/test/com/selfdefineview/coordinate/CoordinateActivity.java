4146bcfbe29f4032c0f4c918fd80687442d64df5
zhanglitao <1397227494@qq.com>
2018-05-18 13:44:49 +0800

    Coordinator的初步使用

--- app/src/main/java/test/com/selfdefineview/coordinate/CoordinateActivity.java
+++ app/src/main/java/test/com/selfdefineview/coordinate/CoordinateActivity.java
@@ -3,6 +3,10 @@ package test.com.selfdefineview.coordinate;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 import test.com.selfdefineview.R;
 
@@ -13,9 +17,42 @@ import test.com.selfdefineview.R;
 
 public class CoordinateActivity extends AppCompatActivity {
 
+    Button mBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coordinate_layout);
+        mBtn = findViewById(R.id.button);
+        mBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_MOVE){
+                    v.setX(event.getRawX()-v.getWidth()/2);
+                    v.setY(event.getRawY() - getStatusBarHeight() - getTitleBarHeight() - v.getHeight()/2);
+                }
+                return true;
+            }
+        });
+    }
+    /**
+     * 获取状态栏的高度
+     * @return
+     */
+    public int getStatusBarHeight(){
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight;
+    }
+    /**
+     * 获取布局标题头部的高度
+     * @return
+     */
+    public int getTitleBarHeight(){
+        int contentViewTop = getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        int titleBarHeight = contentViewTop - getStatusBarHeight();
+        return titleBarHeight;
     }
 }
---
+++ app/src/main/java/test/com/selfdefineview/coordinate/MyBehavor.java
@@ -0,0 +1,51 @@
+package test.com.selfdefineview.coordinate;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by zhangsixia on 18/5/18.
+ */
+
+public class MyBehavor extends CoordinatorLayout.Behavior<TextView> {
+
+    private int screenWidth;
+    public MyBehavor(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        DisplayMetrics display = context.getResources().getDisplayMetrics();
+        screenWidth = display.widthPixels;
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, TextView child, View dependency) {
+        //返回false表示child不依赖dependency，ture表示依赖
+        return dependency instanceof Button;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, TextView child, View dependency) {
+        //根据dependency的位置，设置child的位置
+       /* int top = dependency.getTop();
+        int left = dependency.getLeft();
+        int x = screenWidth - left - child.getWidth();
+        int y = top - 800;
+        setPosition(child, x, y);
+        return true;*/
+        child.setX(dependency.getX()+200);
+        child.setY(dependency.getY()+200);
+        return true;
+
+    }
+    private void setPosition(View v, int x, int y) {
+        CoordinatorLayout.MarginLayoutParams layoutParams = (CoordinatorLayout.MarginLayoutParams) v.getLayoutParams();
+        layoutParams.leftMargin = x;
+        layoutParams.topMargin = y;
+        v.setLayoutParams(layoutParams);
+    }
+
+}
