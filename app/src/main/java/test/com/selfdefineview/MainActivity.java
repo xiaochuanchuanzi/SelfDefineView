2cc3455e154cbab6b2e06c13e65cdba1d295d113
zhanglitao <1397227494@qq.com>
2018-05-22 17:47:29 +0800

    准备拉取分支

--- app/src/main/java/test/com/selfdefineview/MainActivity.java
+++ app/src/main/java/test/com/selfdefineview/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 
 import test.com.selfdefineview.constraint.ConstraintActivity;
 import test.com.selfdefineview.coordinate.CoordinateActivity;
+import test.com.selfdefineview.coordinate.CoordinatorActivity;
 import test.com.selfdefineview.dragtime.DragTimeActivity;
 import test.com.selfdefineview.wifi.WifiActivity;
 import test.com.selfdefineview.xiaokongtu.activity.XiaoKongTu2Activity;
@@ -57,6 +58,15 @@ public class MainActivity extends AppCompatActivity {
      */
     public void enterXiaoKongtu4(View view){
         Intent intent = new Intent(MainActivity.this, CoordinateActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * 点击进入CoordinatorLayout酷炫页面
+     * @param view
+     */
+    public void enterXiaoKongtu42(View view){
+        Intent intent = new Intent(MainActivity.this, CoordinatorActivity.class);
         startActivity(intent);
     }
 
